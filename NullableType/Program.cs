2813819c225f333b3afb45cea0d5e559731342fe
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2018-03-02 20:34:46 +0200

    add simple Nullable Types example

---
+++ NullableType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Methods in nullable types, that are not present in primitive data types.
+            int? someInt = 11;
+            Console.WriteLine(someInt.HasValue);
+            Console.WriteLine(someInt.Value);
+            someInt = null;
+            Console.WriteLine(someInt.HasValue);
+        }
+    }
+}
