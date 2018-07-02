6e07d469b874be0336f0133f61eb5d3ba2ce6b65
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2018-07-02 14:24:44 +0300

    Add ProcessStart(StartAnotherProgram) example and some fix

---
+++ ProcessStart(StartAnotherProgram)/ProcessStart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics; //add this library to use "Process" class
+using System.Threading;
+
+class ProcessStart
+{
+    static void Main(string[] args)
+    {
+        //I.How to start external program from our program?
+        var osType = Environment.OSVersion.ToString().ToLower();
+        var slashOS = string.Empty;
+
+        if (osType.IndexOf("windows") != -1)
+        {  //windows use backslash
+            slashOS = @"\";
+        }
+        else if (osType.IndexOf("unix") != -1)
+        {  //Linux(Unix) use slash
+            slashOS = "/";
+        }
+        else
+        {
+            throw new Exception("OS was not detected correctly!");
+        }
+
+        string relativePath = ".." + slashOS + ".." + slashOS + ".." 
+                                   + slashOS + "DateTime" + slashOS + "bin" + slashOS + "Debug";
+        //NOTE: on Win machine this mean: @"..\..\..\DateTime\bin\Debug"
+
+        //////////////////////////////////////////////////////////////////////////////
+        Process program = new Process();
+        program.StartInfo.WorkingDirectory = relativePath;
+        program.StartInfo.FileName = "DateTime.exe";
+        program.Start();
+
+        try
+        {
+            Console.WriteLine("Is program respond normally? = " + program.Responding);
+            program.WaitForExit(1000 * 60 * 1);    //Wait for 1 minute to exit (alternatively wait for 5 minutes: (1000 * 60 * 5)). 
+            Console.WriteLine("Is program exited normally? =  " + program.HasExited);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error, the program has exited or not responding !!! {0}", ex.StackTrace);
+            Thread.Sleep(1000 * 60 * 1);
+        }
+        //////////////////////////////////////////////////////////////////////////////
+    }
+}
+
+//Process.WaitForExit Method() and more..
+//https://msdn.microsoft.com/en-us/library/fb4aw7b8(v=vs.110).aspx
+//https://msdn.microsoft.com/en-us/library/system.diagnostics.process.exited(v=vs.110).aspx
+//WARNING: WaitForExit() method do NOT work in "Mono" for now !!!
--- ProcessStart(StartAnotherProgram)/Properties/AssemblyInfo.cs
+++ ProcessStart(StartAnotherProgram)/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("colors")]
+[assembly: AssemblyTitle("ProcessStart(StartAnotherProgram)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("collors")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("ProcessStart(StartAnotherProgram)")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c56799d8-ec12-4ab8-8e99-ebef47ded0e0")]
+[assembly: Guid("d93eff70-a40c-499d-aa69-ce5c643066b6")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
