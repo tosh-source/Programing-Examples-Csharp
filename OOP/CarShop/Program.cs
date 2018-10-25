be46c3270d8203b7f4f7a17c8cd118a6a28ef1f1
lin-laptop-test <todor.chupov@gmail.com>
2018-10-25 15:00:05 +0300

    add example "All customer as one (create array of our object)"

--- OOP/CarShop/Program.cs
+++ OOP/CarShop/Program.cs
@@ -9,29 +9,38 @@ namespace CarShop
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //First customer
-            Customer customerIvan = new Customer();
-            customerIvan.FirstName = "Ivan";
-            customerIvan.LastName = "Ivanov";
-            customerIvan.Age = 23;
-            CarModels currentCarForSell = CarModels.BMV;
-            customerIvan.CustomerCarModel = currentCarForSell.ToString();
-            
-            Print(customerIvan.FirstName, customerIvan.LastName, customerIvan.Age, customerIvan.CustomerCarModel);
+		static void Main(string[] args)
+		{
+			//First customer
+			Customer customerIvan = new Customer();
+			customerIvan.FirstName = "Ivan";
+			customerIvan.LastName = "Ivanov";
+			customerIvan.Age = 23;
+			CarModels currentCarForSell = CarModels.BMV;
+			customerIvan.CustomerCarModel = currentCarForSell.ToString();
 
-            //Second customer
-            Customer customerSlavi = new Customer("Slavi", "Hristov", 19, CarModels.Ferrari.ToString());
-            Print(customerSlavi.FirstName, customerSlavi.LastName, customerSlavi.Age, customerSlavi.CustomerCarModel);
+			Print(customerIvan.FirstName, customerIvan.LastName, customerIvan.Age, customerIvan.CustomerCarModel);
 
-            //Third customer
-            Customer customerBlago = new Customer("Blago", "Georgiev", customerCarModel: CarModels.Honda.ToString());  //using of optional parameters: "customerCarModel:" to skip "Age" value
-            Print(customerBlago.FirstName, customerBlago.LastName, customerBlago.Age, customerBlago.CustomerCarModel);
+			//Second customer
+			Customer customerSlavi = new Customer("Slavi", "Hristov", 19, CarModels.Ferrari.ToString());
+			Print(customerSlavi.FirstName, customerSlavi.LastName, customerSlavi.Age, customerSlavi.CustomerCarModel);
 
-        }
+			//Third customer
+			Customer customerBlago = new Customer("Blago", "Georgiev", customerCarModel: CarModels.Honda.ToString());  //using of optional parameters: "customerCarModel:" to skip "Age" value
+			Print(customerBlago.FirstName, customerBlago.LastName, customerBlago.Age, customerBlago.CustomerCarModel);
 
-        static void Print(string FirstName, string LastName, int Age, string CustomerCarModel)
+
+            //BONUS
+			//All customer as one (create array of our object)            
+			Customer[] customers = new Customer[] { customerBlago, customerSlavi, customerIvan};
+			foreach (var customer in customers)
+			{
+				Console.WriteLine();
+				Print(customer.FirstName, customer.LastName, customer.Age, customer.CustomerCarModel);
+			}
+		}
+
+		static void Print(string FirstName, string LastName, int Age, string CustomerCarModel)
         {
             Console.WriteLine(FirstName + " "
                             + LastName
