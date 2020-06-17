d641f6df1a29afa7077735ad0476d7323115092b
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-06-17 14:55:31 +0300

    some changes on Console-SetIn example

--- InputOutput-Techniques2/Program.cs
+++ InputOutput-Techniques2/Program.cs
@@ -5,27 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InputOutput_Techniques2
+namespace InputOutput_Techniques_Console_SetIn
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //I.Simulate the input data  //Симилиране на входните данни в цонзолата
-            //
+            //INFO: How to simulate the input data  //Как да симилиране на входните данни в цонзолата
+            ////////////////////////////////////////////////////
             //ВАЖНО - Този метод симулира ВЕЧЕ въведени данни(от потребител) в конзолата.
             //БЕЛЕЖКА - Много подходящо за използване при тестове. Т.е., преди изпълнението на всеки друг 
             //код, метод и т.н., за да се избегне постоянно въвеждане на едни и същи данни при тестване. 
+
+            //I.Using Console.SetIn
             ////////////////////////////////////////////////////
             var reader = new StringReader("Toshko\nChupov\n31");  //Може да се използва и цитиран низ @""
             Console.SetIn(reader);
             ////////////////////////////////////////////////////
+
+            //Ia. Manipulate strings
             string firstName = Console.ReadLine();
             string lastName = Console.ReadLine();
             int userAge = int.Parse(Console.ReadLine());
             Console.WriteLine(firstName + " " + lastName + " " + userAge);
-
-
+            
         }
     }
 }
