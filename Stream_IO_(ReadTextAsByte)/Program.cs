11ce3463166f7301f7426bf08a7f3b27a315af4c
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-07-06 22:21:22 +0300

    add Stream_IO_(ReadTextAsByte) example

---
+++ Stream_IO_(ReadTextAsByte)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream_IO__ReadTextAsByte_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToTXTfile = @"..\..\testDoc.txt";
+            var byteReader = new FileStream(pathToTXTfile);
+        }
+    }
+}
