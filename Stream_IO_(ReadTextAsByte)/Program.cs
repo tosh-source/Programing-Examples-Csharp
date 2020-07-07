8b18bbd9754c348afed647552d0886b6b8069afb
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-07-07 23:42:29 +0300

    complete Stream_IO (ReadTextAsByte) example

--- Stream_IO_(ReadTextAsByte)/Program.cs
+++ Stream_IO_(ReadTextAsByte)/Program.cs
@@ -11,8 +11,35 @@ namespace Stream_IO__ReadTextAsByte_
     {
         static void Main(string[] args)
         {
-            var pathToTXTfile = @"..\..\testDoc.txt";
-            var byteReader = new FileStream(pathToTXTfile);
+            var pathToTXTfile = @"..\..\testDoc1 (symbols in Cyrillic-UTF8).txt";
+            Console.WriteLine("First text");
+            TextAsByteReader(pathToTXTfile);
+
+            var secondPathToTXTfile = @"..\..\testDoc2 (symbols in Latin-UTF8).txt";
+            Console.WriteLine(Environment.NewLine + "Second Text:");
+            TextAsByteReader(secondPathToTXTfile);
+
+            var thirdPathToTXTfile = @"..\..\testDoc3 (symbols in Cyrillic-UTF8).txt";
+            Console.WriteLine(Environment.NewLine + "Third Text:");
+            TextAsByteReader(thirdPathToTXTfile);
+        }
+
+        private static void TextAsByteReader(string pathToTXTfile)
+        {
+            using (var allTextAsByte = new FileStream(pathToTXTfile, FileMode.Open))
+            {
+                for (int counter = 1; counter <= allTextAsByte.Length; counter++)
+                {
+                    int currenByte = allTextAsByte.ReadByte();
+
+                    Console.Write("{0:X} ", currenByte);
+
+                    //Create new line after every 10 byte. 
+                    //This is just for grouping amount of bytes.
+                    if (counter % 10 == 0) Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
