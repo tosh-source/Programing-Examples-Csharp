e5b91b7f0b41f6abeecab51f2bc4a36b78fc08f7
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-08-24 22:43:47 +0300

    add Generic T PrintArray method

---
+++ OOP/Generic-T-methods/Generic_T_methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_T_methods
+{ 
+    class Generic_T_methods
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void PrintArray<T>(T[] items, string separator = null)
+        {
+            if (separator == null)
+            {
+                separator = ", ";
+            }
+
+            foreach (T item in items)
+            {
+                Console.Write(item);
+                Console.Write(separator);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+//виж видеото на Дончо -> "Дефиниране На Класове - Част 2 - 12 Март 2015 (Дончо)" -> ВРЕМЕ 00:56:35
