766ddb862d562870ca598384511228ff0eccb6b4
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-08-24 23:08:21 +0300

    add Sort T method

--- OOP/Generic-T-methods/Generic_T_methods.cs
+++ OOP/Generic-T-methods/Generic_T_methods.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Generic_T_methods
-{ 
+{
     class Generic_T_methods
     {
         static void Main(string[] args)
@@ -25,6 +25,22 @@ namespace Generic_T_methods
                 Console.Write(separator);
             }
             Console.WriteLine();
+        }
+
+        static void Sort<T>(T[] items) where T : IComparable<T>
+        {
+            for (int firstItem = 0; firstItem < items.Length; firstItem++)
+            {
+                for (int secondItem = firstItem + 1; secondItem < items.Length; secondItem++)
+                {
+                    if (items[secondItem].CompareTo(items[firstItem]) < 0)
+                    {
+                        var tempItem = items[firstItem];
+                        items[firstItem] = items[secondItem];
+                        items[secondItem] = tempItem;
+                    }
+                }
+            }
         }
     }
 }
