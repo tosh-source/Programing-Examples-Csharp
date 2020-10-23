9018aa0d4fd881a489014495798ea0bec757bf17
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-23 22:22:28 +0300

    add jagged array example

--- CopyArraysAndMatrices/CopyArraysAndMatrices.cs
+++ CopyArraysAndMatrices/CopyArraysAndMatrices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections;
 
 namespace CopyArraysAndMatrices
 {
@@ -8,17 +9,33 @@ namespace CopyArraysAndMatrices
         static void Main(string[] args)
         {
             //How to really copy arrays and matrices. Not shallow/reference copy.
+
+            //First variant.
             int[,] matrix =
             {
                 {11, 33},
                 {34, 78}
             };
-
-            int[,] copyOfMatrix = matrix.Clone() as int[,];  //equal to --> (int[,])copyOfMatrix.Clone()
-            copyOfMatrix[0, 0] = 0;
+            int[,] copyOfMatrix = (int[,])matrix.Clone();  //equal to --> matrix.Clone() as int[,]
+            copyOfMatrix[0, 0] = 0;                        //change first element of new array
 
             PrintArrays(copyOfMatrix);
             PrintArrays(matrix);        //There is NO change on FIRST elemnt of the original array/matrix!
+
+
+            //Second variant.
+            int[][] jaggMatrix =
+            {
+                new int[] {23, 76},
+                new int[] {56, 12}
+            };
+
+            int[][] copyOfjaggMatrix = jaggMatrix.Select(x => x.ToArray()).ToArray();  //Lambda can copy ONLY one dimensional array or jagged arrays!
+            copyOfjaggMatrix[1][0] = 0;                                                //change some element of new array
+            //int[][] copyOfjaggMatrix2 = jaggMatrix.Clone() as int[][];  //equal to --> (int[][])jaggMatrix.Clone()
+
+            PrintJaggArrays(copyOfjaggMatrix);
+            PrintJaggArrays(jaggMatrix);       //There is NO changed elemnts of the original array/matrix!
         }
 
         public static void PrintArrays(int[,] arrayToPrint)
@@ -28,6 +45,19 @@ namespace CopyArraysAndMatrices
                 for (int cols = 0; cols < arrayToPrint.GetLength(1); cols++)
                 {
                     Console.Write(arrayToPrint[rows, cols] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintJaggArrays(int[][] arrayToPrint)
+        {
+            for (int rows = 0; rows < arrayToPrint.Length; rows++)
+            {
+                for (int cols = 0; cols < arrayToPrint[rows].Length; cols++)
+                {
+                    Console.Write(arrayToPrint[rows][cols] + " ");
                 }
                 Console.WriteLine();
             }
