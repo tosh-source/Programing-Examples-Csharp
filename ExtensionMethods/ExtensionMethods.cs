0c9e44f9f74571a01e34b07dc26add9eac856092
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-22 23:30:29 +0200

    Add ForEach loop in collection as extension method.

---
+++ ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> actionMethod)
+        {
+            foreach (var currentItem in collection)
+            {
+                actionMethod(currentItem);
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            //Using Anonymous Type.
+            var students = new[]
+            {
+                new { FirstName = "Jon", LastName = "Brown", Age = 23 },
+                new { FirstName = "Ivan", LastName = "Stefanov", Age = 19 },
+                new { FirstName = "Vera", LastName = "Georgieva", Age =  45 },
+                new { FirstName = "Vera", LastName = "Aleksieva", Age = 17 }
+            };
+
+            //Creating extension method and add "ForEach" loop functionality in collections.
+            students.ForEach(Console.WriteLine);
+        }
+    }
+}
