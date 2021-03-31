7799d25ce05c6523f634e05649790ea90c509f4c
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-01 00:50:53 +0300

    Implement GetEnumerator with Yield.

---
+++ OOP/ImplementGetEnumeratorWithYield/CatsEnumerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImplementGetEnumeratorWithYield
+{
+    class CatsEnumerator<T> : IEnumerable<T>
+    {
+        public T[] GetCats { get; set; }
+
+        public CatsEnumerator()
+        {
+        }
+        public CatsEnumerator(T[] setT)
+        {
+            this.GetCats = setT;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.GetCats.Length; i++)
+            {
+                yield return this.GetCats[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+//See video in time: |2:09:50| "Обща система от типове в .NET (CTS) - 25 март 2015 - Ивайло"
---
+++ OOP/ImplementGetEnumeratorWithYield/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyCat;
+
+namespace ImplementGetEnumeratorWithYield
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            Cat[] cats =
+                {
+                    new Cat { Name = "Gosho", Color = "White" },
+                    new Cat { Name = "John", Color = "Black"},
+                    new Cat { }
+                };
+
+            CatsEnumerator<Cat> catsEnumerator = new CatsEnumerator<Cat>();
+            catsEnumerator.GetCats = cats;
+
+            //Use "yield" implementation in foreach loop.
+            foreach (var cat in catsEnumerator)
+            {
+                Console.WriteLine(cat.Name);
+            }
+        }
+    }
+}
