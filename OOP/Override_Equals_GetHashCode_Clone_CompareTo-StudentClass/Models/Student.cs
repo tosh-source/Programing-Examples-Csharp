d9b844e3702b723aa7e13797707aa95ccd8de2c9
Tosh <tosh@example.com>
2021-10-22 21:34:40 +0300

    Add new example Override_Equals_GetHashCode_Clone_CompareTo-StudentClass.

---
+++ OOP/Override_Equals_GetHashCode_Clone_CompareTo-StudentClass/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_Equals_GetHashCode_Clone_CompareTo_StudentClass.Models
+{
+    class Student
+    {
+    }
+}
---
+++ OOP/Override_Equals_GetHashCode_Clone_CompareTo-StudentClass/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Override_Equals_GetHashCode_Clone_CompareTo_StudentClass.Models;
+
+namespace Override_Equals_GetHashCode_Clone_CompareTo_StudentClass
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
