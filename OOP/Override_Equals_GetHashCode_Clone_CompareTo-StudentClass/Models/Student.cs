3ca6f5725f5e0a8b02443c4a44894731a1a37a89
Tosh <tosh@example.com>
2021-10-22 23:40:13 +0300

    complete Equals and GetHashCode

--- OOP/Override_Equals_GetHashCode_Clone_CompareTo-StudentClass/Models/Student.cs
+++ OOP/Override_Equals_GetHashCode_Clone_CompareTo-StudentClass/Models/Student.cs
@@ -8,5 +8,50 @@ namespace Override_Equals_GetHashCode_Clone_CompareTo_StudentClass.Models
 {
     class Student
     {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int SocialSecurityNumber { get; set; } //Student Social Security Number (SSN. E.g. "000-00-0000" 
+
+        public Student() : this(string.Empty, string.Empty, string.Empty, 0)
+        {
+        }
+
+        public Student(string firstName, string middleName, string lastName, int socialSecurityNumber)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            this.SocialSecurityNumber = SocialSecurityNumber;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var student = obj as Student;
+
+            if (student != null &&
+                   this.FirstName == student.FirstName &&
+                   this.MiddleName == student.MiddleName &&
+                   this.LastName == student.LastName &&
+                   this.SocialSecurityNumber == student.SocialSecurityNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 743017444;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(MiddleName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(LastName);
+            hashCode = hashCode * -1521134295 + SocialSecurityNumber.GetHashCode();
+            return hashCode;
+        }
     }
 }
