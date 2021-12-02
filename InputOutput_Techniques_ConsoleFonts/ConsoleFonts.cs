7ab7e173d030cf7099ade4b571c6aeee37f03b2c
Tosh <tosh@example.com>
2021-12-03 01:48:14 +0200

    add console font example

---
+++ InputOutput_Techniques_ConsoleFonts/ConsoleFonts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InputOutput_Techniques_ConsoleFonts
+{
+    class ConsoleFonts
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            //Use current font and try to print Cyrillic symbols. <--(will NOT work)
+            Console.WriteLine(Console.OutputEncoding);
+            Console.WriteLine("АБВГДЕЖЗ");
+
+            //Change font to UTF8 and print Cyrillic symbols.
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(Console.OutputEncoding);
+            Console.WriteLine("АБВГДЕЖЗ");
+
+
+            //Change console width and height.
+            Console.WindowWidth = 50;
+            Console.WindowHeight = 25;
+
+            Console.BufferWidth = 50;
+            Console.BufferHeight = 25;
+        }
+    }
+}
