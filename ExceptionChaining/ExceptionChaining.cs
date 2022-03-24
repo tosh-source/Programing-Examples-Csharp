073e7b6da2936f994a9e2b3e09964aeee28dd787
Tosh <tosh@example.com>
2022-03-24 17:36:35 +0200

    Add Exception Chaining example.

---
+++ ExceptionChaining/ExceptionChaining.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionChaining
+{
+    class ExceptionChaining
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FirstChainedMethod();
+            }
+            catch (Exception generalEx)
+            {
+                Console.WriteLine(generalEx.Message);
+            }
+        }
+
+        private static void FirstChainedMethod()
+        {
+            try
+            {
+                SecondChainedMethod();
+            }
+            catch (InvalidOperationException invlidEx)
+            {
+                throw new InvalidOperationException("Test SECOND exception!" +
+                                                    $"\n{invlidEx.Message}");
+            }
+        }
+
+        private static void SecondChainedMethod()
+        {
+            //throw new NotImplementedException("Test FOUR exception!");  //If uncomment it, this exception will be caught directly by "Exception" class, not by "InvalidOperationException"!
+
+            throw new InvalidOperationException("Test THIRD exception!");
+        }
+    }
+}
