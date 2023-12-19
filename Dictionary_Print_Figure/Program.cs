700b752571e9123a4d24cba67e42b54a79f4fc23
Tosh <tosh@example.com>
2023-12-19 20:33:29 +0200

    Add new example of using Dictionary and show how to store figure pattern and print selected one.

---
+++ Dictionary_Print_Figure/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Print_Figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmpDict = new Dictionary<string, byte[,]>();
+
+            tmpDict.Add("first figure", new byte[,]
+            {
+                {0,0,0,0,0,0,0,0,0 },
+                {0,0,0,0,1,0,0,0,0 },
+                {0,0,0,1,1,1,0,0,0 },
+                {0,1,1,0,1,0,1,1,0 },
+                {0,1,1,1,0,1,1,1,0 },
+                {0,1,1,1,1,1,1,1,0 },
+                {0,0,1,1,1,1,1,0,0 },
+                {0,0,1,1,1,1,1,0,0 },
+                {0,0,0,0,0,0,0,0,0 },
+            });
+            tmpDict.Add("second figure", new byte[,]
+            {
+                {0,0,0,0,0,0,0,0,0 },
+                {0,0,0,0,1,1,0,0,0 },
+                {0,0,0,1,1,1,1,0,0 },
+                {0,0,1,1,1,0,1,0,0 },
+                {0,0,0,1,0,1,1,0,0 },
+                {0,0,0,0,1,1,1,0,0 },
+                {0,0,0,1,1,1,0,0,0 },
+                {0,0,1,1,1,1,1,0,0 },
+                {0,0,0,0,0,0,0,0,0 },
+            });
+
+            var toPrint = tmpDict["first figure"];
+            PrintMethod(toPrint);
+
+            toPrint = tmpDict["second figure"];
+            PrintMethod(toPrint);
+        }
+
+        private static void PrintMethod(byte[,] toPrint)
+        {
+            for (int row = 0; row < toPrint.GetLength(0); row++)
+            {
+                for (int col = 0; col < toPrint.GetLength(1); col++)
+                {
+                    if (toPrint[row, col] != 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
